3e2ed88bb32544ec9abbb023a4748fe5ab49ff41
Cyrillo46 <yahooisbetter46@gmail.com>
2023-07-02 12:55:41 -0400

    removed handle click

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,11 +1,11 @@
 import Starter from "./tutorial/01-useState/starter/01-error-example";
-import ErrorExample from "./tutorial/01-useState/starter/ErrorExample";
+import UseStateBasics from "./tutorial/01-useState/starter/02-useState-basics";
 
 function App() {
   return (
     <div className="container">
       {/* <Starter /> */}
-      <ErrorExample />
+      <UseStateBasics />
     </div>
   );
 }
