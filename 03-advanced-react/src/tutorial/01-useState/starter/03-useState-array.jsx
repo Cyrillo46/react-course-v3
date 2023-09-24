18c1c22ed8ab509c530677d47de2b484e11fa127
Cyrillo46 <yahooisbetter46@gmail.com>
2023-09-24 17:28:05 -0400

    react data challenge

--- 03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,16 @@
+import { data } from "../../../data.js";
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+
+  return (
+    <div>
+      {people.map((person) => {
+        const { id, name } = person;
+        return <h4 key={id}>{name}</h4>;
+      })}
+    </div>
+  );
 };
 
 export default UseStateArray;
