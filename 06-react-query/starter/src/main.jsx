44f696f2cf0b1ed84164313b0bf5d60951b731be
Cyrillo46 <yahooisbetter46@gmail.com>
2024-07-27 09:50:21 -0400

    minor changes

--- 06-react-query/starter/src/main.jsx
+++ 06-react-query/starter/src/main.jsx
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "react-toastify/dist/ReactToastify.css";
-import { QueryCient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new queryClient();
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
